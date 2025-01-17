faffd3db84b617b83a002cd1ddedd53c6904c9c9
Evis201 <83975305+Evis201@users.noreply.github.com>
2025-01-17 12:47:26 +0100

    Init

---
+++ TP2/member.py
@@ -0,0 +1,10 @@
+# Path: TP2/member.py
+
+member = [
+    {
+        "first_name": "Eladji",
+        "last_name": "AhBON",
+        "gender": "homme",
+        "age": 45,
+        "role": "commandant"
+    }
---
+++ TP2/operator.py
@@ -0,0 +1,10 @@
+# Path: TP1/crew.py
+
+crew = [
+    {
+        "first_name": "Eladji",
+        "last_name": "AhBON",
+        "gender": "homme",
+        "age": 45,
+        "role": "commandant"
+    },
