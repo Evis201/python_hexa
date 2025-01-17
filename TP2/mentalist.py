3d5f07896d074e72c10bd007b48c4d481ff82e54
Evis201 <83975305+Evis201@users.noreply.github.com>
2025-01-17 13:27:17 +0100

    Add Mentalist class and enhance Operator actions with role-specific behavior

--- TP2/mentalist.py
+++ TP2/mentalist.py
@@ -1,1 +1,37 @@
 # Path: TP2/mentalist.py
+
+class Member:
+    def __init__(self, name):
+        self.name = name
+
+class Operator(Member):
+    def __init__(self, name, role):
+        super().__init__(name)
+        self.role = role
+        self.experience = 0
+
+    def act(self):
+        print(f"{self.name} {self.role} le vaisseau.")
+
+class Mentalist(Member):
+    def __init__(self, name):
+        super().__init__(name)
+        self.mana = 100
+
+    def get_mana(self):
+        return self.mana
+
+    def set_mana(self, mana):
+        self.mana = mana
+
+    def act(self, operator):
+        if self.mana >= 20:
+            self.mana -= 20
+            operator.act()
+            print(f"{self.name} manipule le membre {operator.name}")
+        else:
+            print(f"{self.name} n'a pas assez de mana pour le manipuler {operator.name}")
+
+    def recharge_mana(self):
+        self.mana = min(self.mana + 50, 100)
+        print(f"{self.name} a rechargé son mana. Mana actuel: {self.mana}.")
--- TP2/operator.py
+++ TP2/operator.py
@@ -8,7 +8,7 @@ class Operator(Member):
     def __init__(self, name, role):
         super().__init__(name)
         self.role = role
-        self.experience = 0 # 0 par défaut pour un nouvel opérateurs
+        self.experience = 0 # 0 par défaut pour un nouvel opérateur
 
     def get_role(self):
         return self.role
@@ -27,3 +27,13 @@ class Operator(Member):
 
     def gain_experience(self):
         self.experience += 1
+        
+    def act(self):
+        if self.role == "pilote":
+            print(f"{self.name} pilote le vaisseau.")
+        elif self.role == "technicien":
+            print(f"{self.name} répare une partie du vaisseau.")
+        elif self.role == "concierge":
+            print(f"{self.name} nettoie les toilettes du vaisseau.")
+        else:
+            print(f"{self.name} {self.role} le vaisseau.")
